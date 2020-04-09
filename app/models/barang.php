c5b953ac5b07889c5872792e468f9a1592c1e15a
mrzf833 <45997345+mrzf833@users.noreply.github.com>
2020-04-09 15:10:04 +0700

    baru

--- app/models/barang.php
+++ app/models/barang.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class barang extends Model
 {
+    protected $dates = ['exp_barang'];
     protected $fillable = ['id','nama','input_barang','exp_barang'];
 }
